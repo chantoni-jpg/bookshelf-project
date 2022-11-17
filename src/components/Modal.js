3aa81176dd8bf97fb7157e84fad4c996689016da
chantoni-jpg <78805182+chantoni-jpg@users.noreply.github.com>
2022-11-16 21:08:53 -0500

    added card and delete function on card

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,37 @@
 import { Modal, Button, Form, Grid } from "semantic-ui-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import {
-  getDoc,
+  getDocs,
   addDoc,
   collection,
   updateDoc,
   deleteDoc,
+  doc,
 } from "firebase/firestore";
 
 const AddBook = () => {
+  const [bookName, setBookName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [open, setOpen] = useState(false);
+  const addBookCollection = collection(db, "books");
+  const createBook = async () => {
+    await addDoc(addBookCollection, {
+      book: bookName,
+      fname: firstName,
+      lname: lastName,
+    });
+  };
+  /*   useEffect(() => {
+    const getBooks = async () => {
+      const booksData = await getDocs(addBookCollection);
+      setBooks(
+        booksData.docs.map((doc) => ({ ...doc.booksData(), id: doc.id }))
+      );
+    };
+    getBooks();
+  }, []); */
   return (
     <Modal
       onClose={() => setOpen(false)}
@@ -26,17 +47,34 @@ const AddBook = () => {
               <Form>
                 <Form.Field>
                   <label>Name of Book:</label>
-                  <input placeholder="Name of book..." />
+                  <input
+                    placeholder="Name of book..."
+                    onChange={(event) => {
+                      setBookName(event.target.value);
+                    }}
+                  />
                 </Form.Field>
                 <Form.Field>
                   <label>Author's First Name:</label>
-                  <input placeholder="Author's First Name..." />
+                  <input
+                    placeholder="Author's First Name..."
+                    onChange={(event) => {
+                      setFirstName(event.target.value);
+                    }}
+                  />
                 </Form.Field>
                 <Form.Field>
                   <label>Author's Last Name:</label>
-                  <input placeholder="Author's Last Name..." />
+                  <input
+                    placeholder="Author's Last Name..."
+                    onChange={(event) => {
+                      setLastName(event.target.value);
+                    }}
+                  />
                 </Form.Field>
-                <Button type="submit">Add Book</Button>
+                <Button type="submit" onClick={createBook}>
+                  Add Book
+                </Button>
               </Form>
             </Grid.Column>
           </Grid.Row>
--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -1,5 +1,23 @@
-import { Grid, Segment } from "semantic-ui-react";
+import { Card, Button, Segment } from "semantic-ui-react";
+import { useState, useEffect } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
 function Shelves() {
+  const deleteBook = async (id) => {
+    const bookDoc = doc(db, "books", id);
+    await deleteDoc(bookDoc);
+  };
+
+  const [books, setBooks] = useState([]);
+  const addBookCollection = collection(db, "books");
+  useEffect(() => {
+    const getBooks = async () => {
+      const data = await getDocs(addBookCollection);
+      setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data);
+    };
+    getBooks();
+  });
   return (
     <div className="shelves">
       <Segment inverted color="grey" padded="very">
@@ -11,6 +29,29 @@ function Shelves() {
       <Segment inverted color="grey" padded="very">
         Finished
       </Segment>
+      <div>
+        {books.map((book) => {
+          return (
+            <Card>
+              <Card.Content>
+                Title of Book: {book.book}
+                <br></br>
+                Author's First Name: {book.fname}
+                <br></br>
+                Author's Last Name: {book.lname}
+                <Button>Edit Card</Button>
+                <Button
+                  onClick={() => {
+                    deleteBook(book.id);
+                  }}
+                >
+                  Delete Card
+                </Button>
+              </Card.Content>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 }
